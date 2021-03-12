f18fd31f02bec8c9e226f5d96af6f56302db4c88
Francis Bonacic <francisbonacic@franciss-mbp.lan>
2021-03-12 11:39:52 -0600

    initial push

--- api/server.js
+++ api/server.js
@@ -4,4 +4,22 @@ const server = express();
 // Complete your server here!
 // Do NOT `server.listen()` inside this file!
 
+//Routers 
+const actionRouter = require('./actions/actions-router');
+const projectRouter = require('./projects/projects-router');
+
+//Use
+server.use(express.json());
+server.use('/api/actions', actionRouter);
+server.use('/api/projects', projectRouter);
+
+
+//testing out my endpoint & server//
+server.get('/', (req, res) => {
+    res.send(`
+      <h2>Sprint Challenge</h2>
+      <p>Build a Web API!</p>
+    `);
+  });
+
 module.exports = server;
